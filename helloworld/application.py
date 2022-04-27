b5c70827311a32c4ffe192342d65a6a36da21438
RenzoPederzoli <renzo-1999@live.com>
2022-04-27 18:36:29 -0400

    demo ready

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,17 +1,52 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+import time, os
+from flask import Flask, Response, request, send_file   
+from subprocess import run
+from flask_cors import CORS
+
 import optparse
 
 application = Flask(__name__)
+CORS(application)
+
+def sendFileServer(f):
+    return run(["scp", './helloworld/'+f, "student_guest@rvc.eng.miami.edu:/home/student_guest/"])
+
+def getFileServer():
+    time_limit = 15 # 20 sec from now.
+    check_interval = 1 # seconds between checking for the file.
+
+    now = time.time()
+    last_time = now + time_limit
+
+    while time.time() <= last_time:
+        if run(["scp", "student_guest@rvc.eng.miami.edu:/home/student_guest/output.jpg", "./helloworld"]).returncode == 0:
+            print('received output file')
+            return 0
+        else:
+            time.sleep(check_interval)
+    return 1;
 
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Helo World'}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+@application.route('/upload_file', methods=['GET', 'POST'])
+def saveImage():
+    f = request.files['file']
+    f.filename = "test.jpg"
+    f.save('./helloworld/'+f.filename)
+    sendFileServer(f.filename)
+    r = getFileServer()
+    if r == 1:
+        return send_file('error-img.png')
+    else:
+        return send_file('output.jpg')
 
 if __name__ == '__main__':
     default_port = "80"
